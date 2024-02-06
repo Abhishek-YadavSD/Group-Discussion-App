99a86d21efc131687df9c3c4b05e4d822e6feae4
Abhishek Yadav <124059750+Abhishek-YadavSD@users.noreply.github.com>
2024-02-06 12:50:08 +0530

    updating folders

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,1 +1,15 @@
-hello home
+<html>
+    <link rel="stylesheet" href="../public/css/home.css">
+<body>
+   
+    <nav class="d-flex">
+        <ul>
+            <img src="" alt="icon">
+            <li><a href="/">Home</a></li>
+            <li><a href="/groups">Groups</a></li>
+            <li><a href="/knowladge">Knowladge</a></li>
+            <li><a href="/debate">Debate</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
